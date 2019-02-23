84d892bea31dd04a5eded6e809c4f1c392696caf
estefaniamuller <47525203+estefaniamuller@users.noreply.github.com>
2019-02-23 13:06:28 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (4).js
+++ 5-InstruccionWhile/jsIteraciones (4).js
@@ -1,17 +1,12 @@
+
 function Mostrar()
 {
-
-	var numero = prompt("ingrese un número entre 0 y 9.");
-
-	while(!( numero >= 0 && numero <= 9) ){
-
-		numero = prompt("no ha ingreso un numero correcto, ingreselo nuevamente. ");
-		}
+	var numero = prompt ("ingrese un numero entre 0 y 9. ");
+	while (!(numero >= 0 && numero <= 9) )
+	{
+		alert ("No ha ingresado un numero correcto. ");
+		numero = prompt("ingrese un numero entre 0 y 9. ");
+	}
 		document.getElementById("Numero").value = numero;
+}
 
-
-
-
-
-
-}//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (9).js
+++ 5-InstruccionWhile/jsIteraciones (9).js
@@ -3,14 +3,37 @@ function Mostrar()
 
 	var contador=0;
 	// declarar variables
-	
-	var respuesta='si';
+	var numero;
+	var maximo;
+	var minimo;
+	var respuesta = 'si';
+	var cantidad = 0;
 
-	while(respuesta!='no')
+	while(respuesta != 'no')
 	{
-		
-	
-	}
+		numero = prompt("Ingrese un numero");
+        if (cantidad == 0)
+        	{ 
+        		maximo = numero;
+        	    minimo = numero;
+        } 
+        else 
+        {
+        	if (numero > maximo)
+        {  
+	        maximo = numero;
+	    }
+	    if (numero < minimo)
+        { 
+             minimo = numero;
+        }         	
+	cantidad++;
+	respuesta = prompt("Desea ingresar otro numero?");
+    }
+    document.getElementById("minimo").value;
+    document.getElementById("maximo").value;
+     
+}
 
 
 
